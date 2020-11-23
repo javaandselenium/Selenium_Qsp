2d908247885e298338f23acd98791bf29d4ace23
QSP <QSP@DESKTOP-EVK9GMC>
2020-11-23 15:44:36 +0530

    Setproperty

---
+++ src/basics/Test0.java
@@ -0,0 +1,18 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");	
+    ChromeDriver driver=new ChromeDriver();
+    
+    System.setProperty("webdriver.gecko.driver","./software/geckodriver.exe");	
+    FirefoxDriver driver1=new FirefoxDriver();
+    
+    
+	}
+
+}
