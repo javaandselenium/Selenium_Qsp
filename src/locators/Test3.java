de6e49a272f3fcf9139258d19c2b69048d1b750a
QSP <QSP@192.168.1.147>
2020-12-02 15:41:48 +0530

    Locators

---
+++ src/locators/Test3.java
@@ -0,0 +1,31 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test3 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		
+		driver.findElement(By.xpath("//div[text()='Login ']")).click();
+		Thread.sleep(5000);
+		String actTitle = driver.getTitle();
+		System.out.println(actTitle);
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		// validation
+		if (actTitle.equals("actiTIME - Enter Time-Track")) {
+			System.out.println("Pass:Home page is dispalyed");
+		} else {
+			System.out.println("Fail:Home page is not dispalyed");
+		}
+		driver.close();
+	}
+
+}
